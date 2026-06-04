89421f9924a6610f4aaf1d633e8d21c8617a507b
princenew873-commits <princenew873@gmail.com>
2026-06-04 17:24:32 +0530

    Create firebase-config.js

---
+++ study-website/firebase-config.js
@@ -0,0 +1,17 @@
+// FIREBASE CONFIG
+
+const firebaseConfig = {
+
+apiKey:"YOUR_API_KEY",
+
+authDomain:"YOUR_DOMAIN",
+
+projectId:"YOUR_PROJECT_ID",
+
+storageBucket:"YOUR_BUCKET",
+
+messagingSenderId:"YOUR_ID",
+
+appId:"YOUR_APP_ID"
+
+};
